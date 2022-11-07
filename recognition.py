ba9f8f259c0a46c75330208aa1192fa3715181ba
FidelComo <99187459+FidelComo@users.noreply.github.com>
2022-11-07 19:04:35 -0300

    limpeza e test do codigo

--- recognition.py
+++ recognition.py
@@ -7,7 +7,7 @@ import os
 
 n=192
 m=168
-r=1600
+r=2
 
 SVD = h5py.File(os.path.join('Matrizes','SVD.h5'), 'r')
 U = SVD['U'][:]
@@ -16,7 +16,7 @@ avgFace = np.reshape(SVD['avg'][:], -1)
 def get_faces(name):
     faces = [np.reshape(crop(os.path.join('Photos', name, i), 'haarcascade_frontalface_default.xml'), -1) for i in os.listdir(os.path.join('Photos', name))]
     if faces:
-        return U[:, 5:5+r].T @ np.vstack([i - avgFace for i in faces]).T
+        return U[:, 6:6+r].T @ np.vstack([i - avgFace for i in faces]).T
     else:
         pass
 
@@ -26,18 +26,23 @@ persons_list = {}
 for i in os.listdir(os.path.join('Photos','Persons')):
     persons_list[i] = get_faces(os.path.join('Persons',i))
 
-distance = {}
-for key, value in persons_list.items():
-    distance[key] = np.apply_along_axis(lambda x: np.linalg.norm(x),0,value-recognize[:,:1]).min()
+distances = []
 
-print(min(distance, key=distance.get))
+for col in recognize.T:
+    distance = {}
+    for key, value in persons_list.items():
+        distance[key] = np.apply_along_axis(lambda x: np.linalg.norm(x-col),0,value).min()
+    distances.append(distance)
 
-modes = [1,2]
-plt.plot(persons_list['Person_1'][modes[0],:],persons_list['Person_1'][modes[1],:],'d',color='k',label='Pessoa 1')
-plt.plot(persons_list['Person_2'][modes[0],:],persons_list['Person_2'][modes[1],:],'^',color='r',label='Pessoa 2')
-plt.plot(recognize[:,:1][modes[0],:],recognize[:,:1][modes[1],:],'^',color='b',label='?')
+for distance in distances:
+    print(min(distance, key=distance.get))
 
 
+modes = [0,1]
+plt.plot(persons_list['ben_afflek'][modes[0],:],persons_list['ben_afflek'][modes[1],:],'d',color='k',label='ben_afflek')
+plt.plot(persons_list['elton_john'][modes[0],:],persons_list['elton_john'][modes[1],:],'^',color='r',label='elton_john')
+plt.plot(recognize[modes[0],0:2],recognize[modes[1],0:2],'d',color='b',label='?')
+plt.plot(recognize[modes[0],2:4],recognize[modes[1],2:4],'^',color='b',label='?')
 
 plt.legend()
 plt.show()
